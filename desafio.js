646f66909294c073b7adc93afdf6f916893ea860
Mateo DM <104037425+mateodm@users.noreply.github.com>
2023-05-01 23:24:09 -0300

    entrega

--- desafio.js
+++ desafio.js
@@ -7,8 +7,8 @@ class ProductManager {
     }
     async getProducts() {
         try {
-        const listaProductos = await fs.promises.readFile(this.#path, "utf-8");
-        return JSON.parse(listaProductos)
+            const listaProductos = await fs.promises.readFile(this.#path, "utf-8");
+            return JSON.parse(listaProductos)
         }
         catch (e) {
             return []
@@ -22,7 +22,7 @@ class ProductManager {
 
         }
         else if (!check) {
-            throw new Error ("Product not found")
+            throw new Error("Product not found")
         }
     }
     async idGenerator(products, product) {
@@ -33,85 +33,92 @@ class ProductManager {
         else {
             return product.id = products[products.length - 1].id + 1;
         };
-        
+
     }
-   async addProduct(title, description, price, thumbnail, code, stock) {
-    const newProduct = {
-        id: 0,
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock,
-    }
-    const producto = await this.getProducts()
+    async addProduct(title, description, price, thumbnail, code, stock) {
+        const newProduct = {
+            id: 0,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+        }
+        const producto = await this.getProducts()
         let productCODEFind = producto.find((product) => product.code === code)
-        if(productCODEFind) {
-        throw new Error("El producto ya existe")
-        } else if (!productCODEFind){
+        if (stock === undefined) {
+            newProduct.stock = 0
+        }
+        if (productCODEFind) {
+            throw new Error("El producto ya existe")
+        } 
+        else if (newProduct.title === undefined || newProduct.description === undefined || newProduct.price === undefined || newProduct.thumbnail === undefined || newProduct.code === undefined) {
+            throw new Error("Falta completar uno de los campos del producto solicitado")
+        } 
+        else if (!productCODEFind) {
             await this.idGenerator(producto, newProduct)
             await fs.promises.writeFile(this.#path, JSON.stringify([...producto, newProduct]))
-         }
-}
-
-async updateProduct(id, selectedParameter, dataToUpdate) {
-    if (id === undefined || selectedParameter === undefined|| dataToUpdate === undefined) {
-        throw new Error ("No ingresaste los parametros correspondientes")
-    }
-    else {
-        const products = await this.getProducts()
-        switch(selectedParameter) {
-            case "title":
-                const titleUpdate = await products.map((products) => products.id === id ? { ...products, title: dataToUpdate } : products
-            );
-                await fs.promises.writeFile(this.#path, JSON.stringify(titleUpdate))
-                await this.getProducts()
-                break;
-            case "description":
-                const descriptionUpdate = await products.map((products) => products.id === id ? {...products, description: dataToUpdate } : products)
-                await fs.promises.writeFile(this.#path,JSON.stringify(descriptionUpdate))
-                break;
-            case "code":
-                const codeUpdate = await products.map((products) => products.id === id ? {...products, code: dataToUpdate } : products)
-                await fs.promises.writeFile(this.#path,JSON.stringify(codeUpdate))
-                break;
-            case "thumbnail":
-                const thumbnailUpdate = await products.map((products) => products.id === id ? {...products, thumbnail: dataToUpdate } : products)
-                await fs.promises.writeFile(this.#path,JSON.stringify(thumbnailUpdate))
-            break;
-            case "price":
-                if (!isNaN(dataToUpdate) === false) {
-                    console.log(dataToUpdate)
-                    throw new Error ("En el caso de querer actualizar price, tienes que ingresar un numero.")
-                }
-                else {
-                    const priceUpdate = await products.map((products) => products.id === id ? {...products, price: dataToUpdate } : products)
-                    await fs.promises.writeFile(this.#path,JSON.stringify(priceUpdate))
-                }
-                break;
-            case "stock":
-                if (!isNaN(dataToUpdate) === false) {
-                    throw new Error ("En el caso de querer actualizar stock, tienes que ingresar un numero.")
-                    }
-                    else {
-                        const stockUpdate = await products.map((products) => products.id === id ? {...products, stock: dataToUpdate } : products)
-                        await fs.promises.writeFile(this.#path,JSON.stringify(stockUpdate))
-                    }
-                    break;
         }
     }
-}
-async deleteProduct(id) {
-    let producto = await this.getProducts()
-    let deleted = producto.find((producto) => producto.id === id)
-    if(deleted) {
 
-    } else if (!deleted) {
-        throw new Error("El id indicado no coincide con ninguno existente")
+    async updateProduct(id, selectedParameter, dataToUpdate) {
+        if (id === undefined || selectedParameter === undefined || dataToUpdate === undefined) {
+            throw new Error("No ingresaste los parametros correspondientes")
+        }
+        else {
+            const products = await this.getProducts()
+            switch (selectedParameter) {
+                case "title":
+                    const titleUpdate = await products.map((products) => products.id === id ? { ...products, title: dataToUpdate } : products
+                    );
+                    await fs.promises.writeFile(this.#path, JSON.stringify(titleUpdate))
+                    await this.getProducts()
+                    break;
+                case "description":
+                    const descriptionUpdate = await products.map((products) => products.id === id ? { ...products, description: dataToUpdate } : products)
+                    await fs.promises.writeFile(this.#path, JSON.stringify(descriptionUpdate))
+                    break;
+                case "code":
+                    const codeUpdate = await products.map((products) => products.id === id ? { ...products, code: dataToUpdate } : products)
+                    await fs.promises.writeFile(this.#path, JSON.stringify(codeUpdate))
+                    break;
+                case "thumbnail":
+                    const thumbnailUpdate = await products.map((products) => products.id === id ? { ...products, thumbnail: dataToUpdate } : products)
+                    await fs.promises.writeFile(this.#path, JSON.stringify(thumbnailUpdate))
+                    break;
+                case "price":
+                    if (!isNaN(dataToUpdate) === false) {
+                        console.log(dataToUpdate)
+                        throw new Error("En el caso de querer actualizar price, tienes que ingresar un numero.")
+                    }
+                    else {
+                        const priceUpdate = await products.map((products) => products.id === id ? { ...products, price: dataToUpdate } : products)
+                        await fs.promises.writeFile(this.#path, JSON.stringify(priceUpdate))
+                    }
+                    break;
+                case "stock":
+                    if (!isNaN(dataToUpdate) === false) {
+                        throw new Error("En el caso de querer actualizar stock, tienes que ingresar un numero.")
+                    }
+                    else {
+                        const stockUpdate = await products.map((products) => products.id === id ? { ...products, stock: dataToUpdate } : products)
+                        await fs.promises.writeFile(this.#path, JSON.stringify(stockUpdate))
+                    }
+                    break;
+            }
+        }
     }
+    async deleteProduct(id) {
+        let producto = await this.getProducts()
+        let deleted = producto.find((producto) => producto.id === id)
+        if (deleted) {
 
-}
+        } else if (!deleted) {
+            throw new Error("El id indicado no coincide con ninguno existente")
+        }
+
+    }
 }
 
 
@@ -120,13 +127,17 @@ async deleteProduct(id) {
 async function test() {
     const manager = new ProductManager("./products.json");
 
-
-await manager.addProduct("Producto5", "Descripcion", 400, "img.jpg", "148")
-await manager.addProduct("Producto6", "Descripcion", 500, "img.jpg", "150")
-await manager.addProduct("Producto7", "Descripcion", 500, "img.jpg", "155")
-await manager.addProduct("Producto8", "Descripcion", 500, "img.jpg", "156")
-await manager.updateProduct(2, "description", "Updated Product")
-await manager.getProductByID(3)
+    /* Agregar productos y autoincrementar ID´s */
+    await manager.addProduct("Producto5", "Descripcion", 400, "img.jpg", "148")
+    await manager.addProduct("Producto6", "Descripcion", 500, "img.jpg", "150")
+    await manager.addProduct("Producto7", "Descripcion", 500, "img.jpg", "155")
+    await manager.addProduct("Producto8", "Descripcion", 500, "img.jpg", "156")
+    /* ERROR FORZADO DE QUE FALTA UN CAMPO */
+    await manager.addProduct("Producto8", "Descripcion", 500, "img.jpg")
+    /* PRUEBA DE UPDATE */
+    await manager.updateProduct(2, "description", "Updated Product")
+    /* PRUEBA OBTENER PRODUCTO X ID */
+    await manager.getProductByID(3)
 
 }
 
