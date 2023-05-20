4886b4c93f185ca8cf562d402a411380dbbc2826
Mateo DM <104037425+mateodm@users.noreply.github.com>
2023-05-20 18:50:05 -0300

    routers

---
+++ src/routes/users.router.js
@@ -0,0 +1,15 @@
+import {Router} from "express"
+const router = Router()
+const users = []
+
+router.get("/",(req, res) => {
+    res.send({users})
+})
+
+router.post("/", (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.send ({ status: "success", users})
+})
+
+export default router
