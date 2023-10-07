d1cd3c96e75d841165d3940446703ba52fa224ac
Mateo DM <104037425+mateodm@users.noreply.github.com>
2023-10-07 02:31:54 -0300

    crypto

--- src/routes/api/checkout.router.js
+++ src/routes/api/checkout.router.js
@@ -4,13 +4,29 @@ import passport from "../../config/passport.js";
 import { ticketService } from "../../service/index.js";
 import generateEmailContent from "../../utils/mailTemplate.js"
 import sendMail from "../../utils/mailer.js"
+import crypto from "crypto"
+import config from "../../config/config.js";
 
 const router = Router()
 
 
 router.post("/ticket", async (req, res) => {
     console.log(req.headers)
-    return res.sendStatus(200)
+    const body = req.body
+    const signature = req.headers["x-signature"];
+    const secretKey = config.mercadopagoKey
+    const expectedSignature = crypto
+    .createHmac("sha256", secretKey)
+    .update(JSON.stringify(body))
+    .digest("hex");
+    if (signature === expectedSignature) {
+        console.log("valido")
+        console.log(body)
+        return res.sendStatus(200)
+    }
+    else {
+        return res.sendStatus(400)
+    }
 })
 
 export default router
