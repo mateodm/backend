85125218bbe8b40a6711bd7c29cb1fe8776bf29c
Mateo DM <104037425+mateodm@users.noreply.github.com>
2023-10-09 00:40:34 -0300

    pruebamp3

--- src/routes/api/checkout.router.js
+++ src/routes/api/checkout.router.js
@@ -5,13 +5,19 @@ import {  productService, cartService, ticketService } from "../../service/index
 import generateEmailContent from "../../utils/mailTemplate.js"
 import sendMail from "../../utils/mailer.js"
 import config from "../../config/config.js";
+import mp from "mercadopago"
 
 const router = Router()
 
 
 router.post("/ticket", async (req, res) => {
-    const body = req.body
-    console.log(req.body)
+    const body = req.body.data
+    let id = body.id
+    let paymentID = await mp.payment.get(id)
+    console.log(paymentID)
+    const preferenceId = paymentID.body.preference_id;
+    let ticket = await ticketService.getBy({code: preferenceId})
+    console.log(ticket)
 })
 
 export default router
