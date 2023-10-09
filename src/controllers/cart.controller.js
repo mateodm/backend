4039d0830fdb2eca70b644ca7fb64e0f3f45b342
Mateo DM <104037425+mateodm@users.noreply.github.com>
2023-10-09 02:23:39 -0300

    pruebamp15

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -141,9 +141,9 @@ class CartController {
                     }
                 }
                 for (const product of successProducts) {
+                    product._doc.quantity = product.quantity;
                     let price = Number(product._doc.price) * Number(product.quantity);
                     amount = Number(amount) + Number(price);
-                    product._doc.quantity = product.quantity
                     const substract = Number(product._doc.stock) - Number(product.quantity);
                     await Products.findByIdAndUpdate(product._doc._id, { stock: substract });
                 }
