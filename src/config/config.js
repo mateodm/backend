ad98a7246e19e12ef29ff9ac3d1e7563edcaf3af
Mateo DM <104037425+mateodm@users.noreply.github.com>
2023-10-20 06:42:40 -0300

    commander

--- src/config/config.js
+++ src/config/config.js
@@ -5,11 +5,10 @@ import { commander } from "../utils/commander.js";
 
 const {mode} = commander.opts()
 
-/* Comentado para que funcione correctamente el deploy en render */
 
-/* dotenv.config({
+dotenv.config({
     path: mode === 'development' ? './.env.development' : './.env.production'
-});  */
+});  
 
 export default  {
     gmail_username: process.env.GMAIL_USER,
